98fdbf87b87a58d1d48599db2ca13a58b42da991
Max <maxbruce@bwrci.org>
2025-08-27 23:50:22 -0500

    Update index.js

--- v2.1/index.js
+++ v2.1/index.js
@@ -1,14 +1,15 @@
 // v2.1/index.js
+const api = require('./basic-threat-detection.js');
 
-const {
-  QSAFPv21SafetyAnalyzer,
-  QSAFPv21ConsensusEngine
-} = require('./basic-threat-detection.js');
-
-module.exports = {
-  safetyAnalyzer: new QSAFPv21SafetyAnalyzer(),
-  consensusEngine: new QSAFPv21ConsensusEngine(),
-  initialize: async function () {
-    console.log('QSAFP v2.1 initialized with Basic Threat Detection');
-  }
-};
+module.exports = Object.assign(
+  {
+    // instance-style (what your current perf-test expects)
+    safetyAnalyzer: new api.QSAFPv21SafetyAnalyzer(),
+    consensusEngine: new api.QSAFPv21ConsensusEngine(),
+    initialize: async function () {
+      console.log('QSAFP v2.1 initialized with Basic Threat Detection');
+    },
+  },
+  // class-style (so code that does `new v21.QSAFPv21Demo()` also works)
+  api
+);
