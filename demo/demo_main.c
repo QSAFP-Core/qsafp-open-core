737c57a636eafc10cd5ba63dc170a57470ddba7f
Max <maxbruce@bwrci.org>
2025-09-12 12:43:06 -0500

    Update demo_main.c

--- demo/demo_main.c
+++ demo/demo_main.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+
+void run_ekl();
+void run_session();
+void run_consensus();
+void run_biometric_quorum();
+
+int main() {
+    run_ekl();
+    run_session();
+    run_consensus();
+    run_biometric_quorum();
+    return 0;
+}
 
