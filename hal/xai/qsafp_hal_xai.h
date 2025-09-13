aa1fd986aff3bc4fd66560af9497195cacd2ac1d
Max <maxbruce@bwrci.org>
2025-09-13 14:19:50 -0500

    Update qsafp_hal_xai.h

--- hal/xai/qsafp_hal_xai.h
+++ hal/xai/qsafp_hal_xai.h
@@ -1,1 +1,12 @@
+#ifndef QSAFP_HAL_XAI_H
+#define QSAFP_HAL_XAI_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+bool xai_hal_init(void);
+bool xai_hal_entropy(uint8_t *buf, size_t len);
+uint64_t xai_hal_timestamp(void);
+
+#endif // QSAFP_HAL_XAI_H
 
