bfd648a9aaa43758ceb08563a35b4500d4ebc3fc
Max <maxbruce@bwrci.org>
2025-09-13 23:17:38 -0500

    Update qsafp_hal_xai.c

--- hal/xai/qsafp_hal_xai.c
+++ hal/xai/qsafp_hal_xai.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include "qsafp_hal_xai.h"
 
+// Explicit stub implementation for biometric quorum
 void run_biometric_quorum(void) {
-    printf("[xAI HAL] Biometric quorum stub invoked.\n");
+    printf("[XAI HAL] Biometric quorum stub invoked.\n");
+    fflush(stdout); // force output flush (Windows sometimes buffers)
 }
+
 
 
