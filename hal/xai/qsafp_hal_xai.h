e9959e69a39a250af97938bf07cfc6b926d38238
Max <maxbruce@bwrci.org>
2025-09-13 21:22:37 -0500

    Update qsafp_hal_xai.h

--- hal/xai/qsafp_hal_xai.h
+++ hal/xai/qsafp_hal_xai.h
@@ -1,12 +1,9 @@
 #ifndef QSAFP_HAL_XAI_H
 #define QSAFP_HAL_XAI_H
 
-#include <stdint.h>
-#include <stdbool.h>
+// Declare the stub function for xAI integration
+int run_biometric_quorum();
 
-bool xai_hal_init(void);
-bool xai_hal_entropy(uint8_t *buf, size_t len);
-uint64_t xai_hal_timestamp(void);
+#endif
 
-#endif // QSAFP_HAL_XAI_H
 
